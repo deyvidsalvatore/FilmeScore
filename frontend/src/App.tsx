367ab266377af7bbe03edcd5b273415975b16696
Deyvid Santos <deyvidsantosdatascience@gmail.com>
2023-12-30 18:59:51 -0300

    Configurando rotas

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
+import Lista from "./pages/Lista";
+import Formulario from "./pages/Formulario";
 
 function App() {
   return (
-    <Navbar/>
+    <BrowserRouter>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<Lista /> } />
+          <Route path="/form">
+            <Route path=":filmeId" element={<Formulario />}/>
+          </Route>
+        </Routes>
+    </BrowserRouter>
   );
 }
 
